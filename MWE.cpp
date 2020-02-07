4c29d73755d83d4f6ab0625566f848a82187340c
André Mewes <mewes.andre@gmail.com>
2020-02-07 16:22:35 +0100

    initial mwe with qt and eigen

---
+++ MWE.cpp
@@ -0,0 +1,17 @@
+﻿#include "MWE.h"
+#include <QApplication>
+#include <QWidget>
+#include <Eigen/Geometry>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	QApplication app(argc, argv);
+	QWidget widget;
+	widget.show();
+
+	Eigen::Affine3d aff;
+
+	return app.exec();
+}
